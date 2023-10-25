14a6fe9a39e36095d98a22c38d11eef37173c3a1
yeoseon <ous0902@naver.com>
2023-10-25 22:26:12 +0900

    test

--- Inha-graduate-BE/index.js
+++ Inha-graduate-BE/index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => res.send('linked!'));
+app.listen(8001, () => console.log('Server Up and running at 8001'));
